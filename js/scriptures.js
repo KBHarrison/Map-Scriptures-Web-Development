a3c9141fbf39cd30cc3f352584a44858e0528b27
Kyle Harrison <kylebharrison16@gmail.com>
2020-01-28 20:19:36 -0700

    finished navigateHome and navigateBook functionality

--- js/scriptures.js
+++ js/scriptures.js
@@ -1,15 +1,24 @@
 const Scriptures = (function() {
     "use strict";
-
+/******************************************Constants**********************************
+*/
     let books;
     let volumes;
-
+/******************************************Method Declaration**********************************
+*/
     let ajax;
+    let bookChapterValid;
     let cacheBooks;
     let clearMap;
     let init;
     let initMap;
+    let navigateBook;
+    let navigateChapter;
+    let navigateHome;
     let onHashChanged;
+
+    /******************************************Functions**********************************
+*/
 
     ajax = function(url, successCallback, failureCallback) {
         let request = new XMLHttpRequest();
@@ -30,6 +39,16 @@ const Scriptures = (function() {
         request.onerror = failureCallback;
 
         request.send();
+    };
+    bookChapterValid = function(bookId, chapter) {
+        let book = books[bookId];
+        if (book === "undefined" || chapter < 0 || chapter > book.numChapters) {
+            return false;
+        }
+        if (chapter === 0 && book.numChapters > 0) {
+            return false;
+        }
+        return true;
     };
     cacheBooks = function(callback) {
         volumes.forEach(volume => {
@@ -61,7 +80,7 @@ const Scriptures = (function() {
             data => {
                 books = data;
                 booksLoaded = true;
-
+                console.log(books);
                 if (volumesLoaded) {
                     cacheBooks(callback);
                 }
@@ -69,6 +88,7 @@ const Scriptures = (function() {
         );
         ajax("https://scriptures.byu.edu/mapscrip/model/volumes.php", 
             data => {
+                console.log(data);
                 volumes = data;
                 volumesLoaded = true;
 
@@ -78,9 +98,51 @@ const Scriptures = (function() {
             }
         );
     };
-
+    navigateBook = function(bookId) {
+        console.log("navigate chapter ", bookId);
+    };
+    navigateHome = function(volumeId) {
+        document.getElementById("scriptures").innerHTML = 
+        "<div>The Old Testament</div>" + 
+        "<div>The New Testament</div>" + 
+        "<div>The Book of Mormon</div>" + 
+        "<div>Doctrine and Covenants</div>" + 
+        "<div>The Pearl of Great Price</div>" + volumeId 
+    };
     onHashChanged = function() {
-        console.log("the hash is " + location.hash);
+        let ids = [];
+        if (location.hash !== "" && location.hash.length > 1) {
+            ids = location.hash.slice(1).split(":");
+        }
+        if (ids.length <= 0) {
+            navigateHome();
+        } else if (ids.length === 1) {
+            let volumeId = Number(ids[0]);
+
+            if (volumeId < volumes[0].id || volumeId > volumes.slice(-1).id) {
+                navigateHome();
+            } else {
+                navigateHome(volumeId);
+            }
+        } else if (ids.length >= 2) {
+            let bookId = Number(ids[1]);
+
+            if (books[bookId] === undefined) {
+                navigateHome();
+            } else {
+                if (ids.length === 2) {
+                    navigateBook(bookId);
+                } else {
+                    let chapter = Number(ids[2]);
+
+                    if (bookChapterValid(bookId, chapter)) {
+                        navigateChapter(bookId, chapter);
+                    } else {
+                        navigateHome();
+                    }
+                }
+            }
+        }
     };
 
 return {
